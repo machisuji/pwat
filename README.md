480bf2b084487a06bd8986d7614b7e15c3b5313a
Markus Kahl <machisuji@googlemail.com>
2016-04-11 22:22:51 +0100

    pwat as written by someone who doesn't know rust

---
+++ README.md
@@ -0,0 +1,13 @@
+# pwat
+
+Prints bits of a password at the given 1-based indices.
+
+Example:
+
+```
+> pwat 2 5 8
+Enter password: hallowelt
+lwt
+```
+
+The password not being displayed of course.
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+extern crate rpassword;
+
+use std::io::Write;
+use std::io::stdout;
+use rpassword::read_password;
+use std::env;
+
+fn main() {
+  let args: Vec<String> = env::args().collect();
+
+  if args.len() == 1 {
+      println!("Usage: xo m [n...]");
+      println!("Prints bits of a password at the given 1-based indices.");
+      println!("");
+      println!("example: xo 2 5 8");
+      println!("         (typing in 'hallowelt')");
+      println!(" result: lwt");
+  } else {
+      print!("Enter password: ");
+      stdout().flush();
+      let password = read_password().unwrap();
+
+      for s in args {
+          for n in s.parse::<u32>() {
+              print!("{}", password.chars().nth(n as usize).unwrap());
+          }
+      }
+      println!("");
+  }
+}
